5523448125e9175a637c8a3137b9297c222a683b
Takuto Komazaki <komazarari@users.noreply.github.com>
2019-03-15 16:43:03 +0900

    Update post_chsh.rb

--- recipes/post_chsh.rb
+++ recipes/post_chsh.rb
@@ -4,20 +4,20 @@ execute 'fish -c "fisher add decors/fish-ghq"'
 execute 'fish -c "fisher add rafaelrinaldi/pure"'
 execute 'fish -c "fisher add jethrokuan/z"'
 
-execute 'go get github.com/motemem/ghq' do
+execute 'go get github.com/motemen/ghq' do
   user name
   group name
   not_if "which ghq"
 end
 
-execute 'fish -c "set fish_user_paths $HOME/.local/bin $fish_user_paths"' do
+execute 'fish -c "set -U fish_user_paths $HOME/.local/bin $fish_user_paths"' do
   not_if { ENV['PATH'].include?('/.local/bin') }
 end
 
-execute 'fish -c "set fish_user_paths $HOME/.nodenv/bin $fish_user_paths"' do
+execute 'fish -c "set -U fish_user_paths $HOME/.nodenv/bin $fish_user_paths"' do
   not_if { ENV['PATH'].include?('/.nodenv/bin') }
 end
 
-execute 'fish -c "set fish_user_paths $HOME/.rbenv/bin $fish_user_paths"' do
+execute 'fish -c "set -U fish_user_paths $HOME/.rbenv/bin $fish_user_paths"' do
   not_if { ENV['PATH'].include?('/.rbenv/bin') }
 end
