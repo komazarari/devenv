6b354018a32e67c485e9d2033ecdc6203ece01da
Takuto Komazaki <takuto.komazaki@gree.net>
2021-05-21 11:12:05 +0900

    golang-1.16

--- recipes/default.rb
+++ recipes/default.rb
@@ -65,20 +65,20 @@ package "terraform"
 apt_repository 'golang-backports' do
   uri 'ppa:longsleep/golang-backports'
 end
-package 'golang-1.15' do
+package 'golang-1.16' do
   notifies :run, 'execute[install_golang]'
   notifies :run, 'execute[install_gofmt]'
 end
 
-# golang 1.15
+# golang 1.16
 execute 'install_golang' do
-  command "update-alternatives --install /usr/local/bin/go golang /usr/lib/go-1.15/bin/go 10"
+  command "update-alternatives --install /usr/local/bin/go golang /usr/lib/go-1.16/bin/go 10"
   creates '/usr/local/bin/go'
   action :nothing
 end
 
 execute 'install_gofmt' do
-  command "update-alternatives --install /usr/local/bin/gofmt gofmt /usr/lib/go-1.15/bin/gofmt 10"
+  command "update-alternatives --install /usr/local/bin/gofmt gofmt /usr/lib/go-1.16/bin/gofmt 10"
   creates '/usr/local/bin/gofmt'
   action :nothing
 end
