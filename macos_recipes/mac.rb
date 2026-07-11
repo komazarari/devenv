da6269374f15ac7e3842d2bdb0d1f3f50a8ac1b9
Takuto Komazaki <komazarari@gmail.com>
2026-07-11 13:41:41 +0900

    mac

---
+++ macos_recipes/mac.rb
@@ -0,0 +1,8 @@
+# run as root
+
+homebrew_package 'gh'
+homebrew_package 'ghq'
+homebrew_package 'eza' # ls replacement
+homebrew_package 'bat' # cat replacement
+homebrew_package 'fzf' # fuzzy finder
+homebrew_package 'ripgrep' # grep replacement
